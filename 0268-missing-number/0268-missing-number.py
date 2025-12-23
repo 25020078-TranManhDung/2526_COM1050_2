a4a1e3e0dd2e4e26fe66eaa12540f27d4459b07a
ManhDung <25020078@vnu.edu.vn>
2025-12-23 11:24:40 +0700

    Delete 0268-missing-number directory

--- 0268-missing-number/0268-missing-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
-        return None
-        
