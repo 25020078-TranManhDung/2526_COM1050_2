5fad6ede6c4c9761744efdbc285f1755a2a9b171
ManhDung <25020078@vnu.edu.vn>
2026-01-05 19:36:21 +0700

    solved 448

---
+++ 448.py
@@ -0,0 +1,12 @@
+def findDisappearedNumbers(nums):
+        res = []
+        for num in nums:
+            x = abs(num)
+            idx = x - 1
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
+        return res
+
